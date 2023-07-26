f365a451f44ab49346e53349ad51ca7aca652efe
frimpongOpoku <pongofrimi@gmail.com>
2023-07-26 09:28:23 +0400

    cashincashout page works well, and with backend :fire:

--- app/pages/cash in or out/CashInOrOut.js
+++ app/pages/cash in or out/CashInOrOut.js
@@ -7,9 +7,18 @@ import GButton from '../../components/button/Button';
 import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
 import {colors} from '../../styles';
-import {toggleUniversalModal} from '../../redux/actions/actions';
+import {
+  setGallamseyUser,
+  toggleUniversalModal,
+} from '../../redux/actions/actions';
+import AsDialogBox from '../../components/modal/AsDialogBox';
+import {faCheckCircle} from '@fortawesome/free-solid-svg-icons';
+import WIthIconAndText from '../../components/modal/WIthIconAndText';
+import {apiCall} from '../../api/messenger';
+import {UPDATE_USER_URL} from '../../api/urls';
 
-const CashInOrOut = ({route, user, toggleModal}) => {
+const CashInOrOut = ({route, user, toggleModal, updateUserInRedux}) => {
+  const wallet = user?.wallet || {};
   const [amount, setAmount] = useState(0);
   const value = route.params?.data || 'cashin';
   const modes = {
@@ -29,9 +38,80 @@ const CashInOrOut = ({route, user, toggleModal}) => {
     },
   };
 
-  const takeAction = (value)=>{ 
+  const updateOnBackend = (data, cb) => {
+    apiCall(
+      UPDATE_USER_URL,
+      {
+        body: {id: user?._id, wallet: {...wallet, ...data}},
+      },
+      response => {
+        console.log('RESPONSE ON CASHINOUT PAGE', response);
+        if (!response.success)
+          return console.log('Error on CashInOut Page: ', response.error);
+        updateUserInRedux(response.data);
+        cb && cb();
+      },
+    );
+  };
 
-  }
+  const takeAction = () => {
+    if (!amount) return;
+
+    const options = {
+      cashin: {
+        text: `An amount of GHS ${amount} will be retrieved from your MOMO account into your Gallamsey app wallet. Are you sure?`,
+        congratulations: `Congratulations! An amount of GHS ${amount}  has now been deposited into your Gallamsey wallet, enjoy!`,
+        newBalance: {balance: (wallet?.balance || 0) + Number(amount)},
+      },
+      cashout: {
+        text: `An amount of GHS ${amount} will be sent from your Gallamsey wallet into your MOMO account. Are you sure?`,
+        congratulations: `Congratulations! An amount of GHS ${amount}  has now been deposited into your MOMO acount, enjoy!`,
+        newBalance: {balance: (wallet?.balance || 0) - Number(amount)},
+      },
+    };
+
+    const option = options[value];
+    console.log('thats the options man', option);
+    toggleModal({
+      show: true,
+      component: (
+        <AsDialogBox
+          textOptions={{
+            text: option?.text,
+          }}
+          iconOptions={{icon: faCheckCircle, color: colors.green}}
+          noOptions={{
+            text: 'NO',
+            onPress: () => {
+              toggleModal({show: false});
+              // navigation.navigate('CashInOrOut', {data: 'cashin'});
+            },
+          }}
+          yesOptions={{
+            text: 'YES, GO AHEAD',
+            onPress: () => {
+              // toggleModal({show: false});
+              // navigation.navigate('CashInOrOut', {data: 'cashout'});
+              updateOnBackend(option?.newBalance, () => {
+                toggleModal({
+                  show: true,
+                  component: (
+                    <WIthIconAndText
+                      textOptions={{
+                        text: option?.congratulations,
+                      }}
+                      iconOptions={{icon: faCheckCircle, color: colors.green}}
+                    />
+                  ),
+                });
+                setAmount('');
+              });
+            },
+          }}
+        />
+      ),
+    });
+  };
 
   const mode = modes[value];
   return (
@@ -59,7 +139,7 @@ const CashInOrOut = ({route, user, toggleModal}) => {
           containerStyle={{padding: 0, marginBottom: 10}}
           generics={{keyboardType: 'numeric'}}
         />
-        <GButton>{mode?.btn}</GButton>
+        <GButton onPress={() => takeAction()}>{mode?.btn}</GButton>
       </View>
     </View>
   );
@@ -70,6 +150,7 @@ const mapDispatchToProps = dispatch =>
   bindActionCreators(
     {
       toggleModal: toggleUniversalModal,
+      updateUserInRedux: setGallamseyUser,
     },
     dispatch,
   );
